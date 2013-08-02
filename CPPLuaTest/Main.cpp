cecf337630c97603ac2681945a9b1855978a8826
Cody Duncan <CodyDuncan1260@live.com>
2013-08-01 19:46:21 -0500

    Setup Project as Hello World.

---
+++ CPPLuaTest/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void pressKey()
+{
+	system("PAUSE");
+}
+
+int main(int argc, char **argv)
+{
+	cout << "Hello World!" << endl;
+
+	pressKey();
+}

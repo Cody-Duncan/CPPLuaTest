7e0b6a83e6ea83f2c2fba387e291a91df397b153
Cody Duncan <CodyDuncan1260@live.com>
2013-08-01 23:02:06 -0500

    Errors output to log file and console.

--- CPPLuaTest/Main.cpp
+++ CPPLuaTest/Main.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <fstream>
+#include <time.h>
 #include "lua.hpp"
 
 using namespace std;
 
 lua_State *L;  //global lya state pointer
+FILE* logFile;
 
 
 /// <summary>
@@ -18,6 +21,21 @@ void pressKey()
     system("PAUSE");
 }
 
+
+/// <summary>
+/// Gets the current date and time in YYYY-MM-DD.HH:mm:ss format
+/// </summary>
+/// <returns>current date and time</returns>
+const string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%B-%d.%X", &tstruct);
+
+    return buf;
+}
+
 static int report (lua_State *L, int status) {
     const char *msg;
     if (status) 
@@ -26,6 +44,7 @@ static int report (lua_State *L, int status) {
         if (msg == NULL) 
             msg = "(error with no message)"; 
         fprintf(stderr, "status=%d, %s\n", status, msg);
+        fprintf(logFile, "status=%d, %s\n", status, msg);
         lua_pop(L, 1);                                      //get rid of error message from stack
     }
     return status;
@@ -76,6 +95,9 @@ int main(int argc, char **argv)
 {
     int err;
 
+    logFile = fopen("errorLog.txt", "a");
+    fprintf(logFile, "Starting Log: %s\n", currentDateTime().c_str());
+
     err = initLua("test.lua");
     if(!err)
     {
@@ -83,5 +105,6 @@ int main(int argc, char **argv)
         disposeLua();
     }
 
+    fclose(logFile);
     pressKey();
 }
